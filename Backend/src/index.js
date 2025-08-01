9ae90cc4ce133d20429345a7fca62f8a1ad0a1f8
Prince Singh <imprincestark.45@gmail.com>
2025-08-01 22:14:25 +0530

    second commit

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -13,7 +13,10 @@ const PORT = process.env.PORT || 5001 ;
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
-})); 
+}));
+
+
+
 app.use(cookieParser()) ; 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
