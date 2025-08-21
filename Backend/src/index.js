44e781143f9cd27a1d9536715f0a443451181235
Prince Singh <imprincestark.45@gmail.com>
2025-08-21 16:23:40 +0530

    render issue resolve

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -26,6 +26,6 @@ app.use("/api/messages" , messageRoutes) ;
 
 
 server.listen(5001 , ()=> {
-    console.log("Server is running on port 5001") ; 
+    console.log("Server is running on port 5001 ") ; 
     connectDB() ; 
 }) ; 
